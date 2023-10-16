8523d5138d2905251ed00314c1ab54b4eeab3a2d
j-o-phillips <jphillips105@googlemail.com>
2023-10-16 15:56:46 +0200

    ammended

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -8,7 +8,7 @@ import serverless from "serverless-http";
 import dotenv from "dotenv";
 dotenv.config();
 
-import router from "./routes/routes.js";
+import router from "../../routes/routes.js";
 
 mongoose.connect(`${process.env.DATABASE_URL}`);
 
